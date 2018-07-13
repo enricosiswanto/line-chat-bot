c510fba6135d1d06ab45e57d2e4bf6db06c49640
enricosiswanto <enrico@siswanto.me>
2018-07-13 08:53:49 +0700

    Revision 1.2 commit

--- respond.php
+++ respond.php
@@ -1,4 +1,20 @@
 <?php
+/** This is a PHP script for handling events from LINE Messaging API, 
+ * 
+ * @copyright Copyright (c) 2018, Enrico Siswanto
+ * @version 1.2
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
+
     require_once('line.php');
 
     $channelToken = ''; //Put your line channel token here
